f8e772f608a225dd5c2e7c33c95be778d104cf9f
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-09-07 22:06:23 +0600

    to-do: access each column and generate random true index for each column

--- amaratkhan_assignment2.py
+++ amaratkhan_assignment2.py
@@ -1,17 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # generate matrix with random values in the range (-10,10)
 # 10 classes (rows) by 100 instances (columns)
 array = 10*np.random.uniform(-1,1, (10,100))
-
+print("ARRAY:")
 print(array)
-print(type(array[0,0]))
 
 # choose the right value for each column randomly randint(0,9)
+true_index = np.random.randint(0,9)
+print("TRUE INDEX:")
+print(true_index)
 
 # compute SVM Loss for each and average
+for i in range(0,array.size):
+    for j in range(0,array[i].size):
+        if i == 0 and j % 10 == 0:
+            print(i,j)
+
+
 
 # compute Softmax cross-entropy for each and average
 
+
+
 # repeat the process 50 times
+
+
+
+# plot the averages to the graph
